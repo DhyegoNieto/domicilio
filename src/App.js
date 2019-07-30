9b26aecca8e0cbbc2b7701dad3235479bc70596e
GLOBANT\diego.nieto <diego.nieto@globant.com>
2019-07-29 22:40:53 -0500

    login with auth0, logout fails

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Container } from "reactstrap";
+
+import PrivateRoute from "./components/PrivateRoute";
+import Loading from "./components/Loading";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Products from './components/Products';
+import { useAuth0 } from "./react-auth0-spa";
+
+// styles
+import "./App.css";
+
+// fontawesome
+import initFontAwesome from "./utils/initFontAwesome";
+initFontAwesome();
+
+const App = () => {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <Router>
+      <div id="app" className="d-flex flex-column h-100">
+        <NavBar />
+        <Container className="flex-grow-1 mt-5">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <PrivateRoute path="/profile" component={Profile} />
+            <PrivateRoute path="/products" component={Products}/>
+          </Switch>
+        </Container>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
